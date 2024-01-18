44225b197da1524ba01e8af93787c442531cc982
Nikita <kosheevnikita80@gmail.com>
2024-01-18 19:52:05 +0300

    delete "id" column from task.html

--- src/main/java/com/nikitakosh/TaskManager/controllers/TaskController.java
+++ src/main/java/com/nikitakosh/TaskManager/controllers/TaskController.java
@@ -2,8 +2,6 @@ package com.nikitakosh.TaskManager.controllers;
 
 import com.nikitakosh.TaskManager.models.Task;
 import com.nikitakosh.TaskManager.services.TaskService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
